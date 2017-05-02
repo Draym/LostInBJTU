895eedabec629b988741e95458aa4f058d0b6ecb
andres_k <kevin.andres@epitech.eu>
2017-05-02 18:41:05 +0800

    [add] initial commit

---
+++ web-front/app/scripts/controllers/about.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name LostInBJTUApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the LostInBJTUApp
+ */
+angular.module('LostInBJTUApp')
+  .controller('AboutCtrl', function ($scope, User) {
+  });
